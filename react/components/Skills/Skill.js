3f0a34b328b43025b0950d7284b7ebbe91e9c537
Samuel Grant <samueljegrant@outlook.com>
2018-10-20 16:23:25 +1300

    Requested change

--- react/components/Skills/Skill.js
+++ react/components/Skills/Skill.js
@@ -3,10 +3,6 @@ import OpenMarket from 'components/OpenMarket';
 
 
 class Skills extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return(
             <tr>
