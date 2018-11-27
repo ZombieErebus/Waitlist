f376678bd5e73d74c2319d83ca2a95a1bb0c2af5
Mark Lohstroh <mlohstroh@gmail.com>
2018-11-27 01:12:35 -0600

    adding tooltips for fleet at a glance

--- react/components/fleetGlanceRow.js
+++ react/components/fleetGlanceRow.js
@@ -6,7 +6,9 @@ class FleetGlanceRow extends Component {
             return(
                 <React.Fragment key={index}>
                     <td className="tw35"><img src={`https://image.eveonline.com/Render/${ship.id}_32.png`} alt="Ship Icon" /></td>
-                    <td className="tw20per"><a href="#">{ship.name}</a></td>
+                    <td className="tw20per">
+                        <a href="#" data-toggle="tooltip" data-html="true" title={ship.pilots.join("<br/>")}>{ship.name}</a>
+                    </td>
                     <td>{ship.pilots.length}</td>
                 </React.Fragment>
             );
